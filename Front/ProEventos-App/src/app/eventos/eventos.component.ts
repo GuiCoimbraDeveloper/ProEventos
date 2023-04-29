abc4a73ea8c2bbc13e0dcd6e946ab104f330b4c0
Guilherme Coimbra <guilherme.xavier27@outlook.com>
2023-04-29 12:01:49 -0300

    Seção 5: Angular, Interpolação, Diretiva e binding

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,26 @@ import { Component } from '@angular/core';
 })
 export class EventosComponent {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados:any = [];
+  exibirImagem:boolean=true;
+  private _filtroLista:string = '';
+
+  public get filtroLista(){
+    return this._filtroLista;
+  }
+  public set filtroLista(value:string){
+    this._filtroLista=value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista): this.eventos;
+  }
+
+  filtrarEventos(filtrarPor:string):any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento:any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    ) ;
+  }
 
   constructor(private http: HttpClient){ }
 
@@ -16,9 +35,16 @@ export class EventosComponent {
     this.getEventos();
   }
 
+  alterarImagem(){
+    this.exibirImagem = !this.exibirImagem;
+  }
+
   public getEventos(): void {
     this.http.get('https://localhost:7285/api/eventos').subscribe({
-      next: (response) => this.eventos=response,
+      next: (response) => {
+        this.eventos=response
+        this.eventosFiltrados=response
+      },
       error: (e)=> console.log(e),
       complete: () => console.info('complete')
     });
